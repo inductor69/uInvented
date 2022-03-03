392dcf26612e010a8ce63fb487bf381cdfb7c833
inductor69 <adityakiitdelhi@gmail.com>
2022-03-03 16:11:12 +0530

    updates

--- pages/_app.js
+++ pages/_app.js
@@ -4,7 +4,7 @@ import { ThemeProvider } from "next-themes";
 
 function MyApp({ Component, pageProps }) {
   return (
-    <div className="bg-[#fffbe9]">
+    <div className="bg-gradient-to-r  to-[#757F9A] from-[#bbc0c9]">
       <Component {...pageProps} />
     </div>
   );
