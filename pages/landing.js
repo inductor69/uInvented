fcd7aaf00e70c95430c00e41a25883290192b5cc
inductor69 <adityakiitdelhi@gmail.com>
2022-03-15 14:29:43 +0530

    updates

--- pages/landing.js
+++ pages/landing.js
@@ -306,6 +306,26 @@ export default function Landing() {
       </div>
 
       {/*Our Testimonials*/}
+      <div className="w-full h-full bg-[#000] pb-10 ">
+
+<div className="max-w-3xl p-4 text-gray-800 bg-white rounded-lg shadow-md  shadow-cyan-700/50">
+        <div className="mb-2">
+          <div className="h-3 text-3xl text-left text-indigo-600">“</div>
+          <p className="px-4 text-center text-gray-600">
+            The Landscaping Professionals were quick, courteous and very helpful. They
+            helped me restore my lawn and gardens completely after putting in my deck.
+            I was worried it wouldn’t be done in time for my garden party, but they
+            finished the job with time to spare!”
+          </p>
+          <div className="h-3 text-3xl text-right text-indigo-600">”</div>
+          <div className="text-center">
+            <h5 className="font-bold text-indigo-600">John Doe</h5>
+            <p className="text-sm text-gray-600">CEO / Founder</p>
+          </div>
+        </div>
+      </div>
+</div>
+
 
       <div className="w-full h-full bg-[#000] pb-10 ">
         <div className="sm:text-5xl text-white bg-black  text-md text-center md:leading-[7rem]  ">
