a232bc55acd696b0b1aadf696fc90c25e00ae9bd
Harshit Bhalla <harshitbhalla0018@gmail.com>
2022-12-25 23:13:32 +0530

    bugs n project

---
+++ components/Projects.js
@@ -0,0 +1,31 @@
+import React from 'react'
+
+export const Projects  = [
+        {
+            title:"Elekrity",
+            description:"Beat procrastination and get your personal goals completed as planned!",
+            link:"/",
+            image:"/images/portfolio.png",
+            bgColor:"[#9df5fd]",
+            controlColor:"[#9969c9]"
+        },
+        {
+            title:"Personal Portfolio",
+            description:"Personal Portfolio showcasing the work of Founders and Designers built with NextJS",
+            link:"/",
+            image:"/images/meet.png",
+            bgColor:"[#F0E4FC]",
+            controlColor:'yellow-600'
+
+        },
+        {
+            title:"Personal Portfolio",
+            description:"Personal Portfolio showcasing the work of Founders and Designers built with NextJS",
+            link:"/",
+            image:"/images/relax.png",
+            bgColor:"[#ffefdf]",
+            controlColor:'[#9969c9]'
+        }]
+
+
+
