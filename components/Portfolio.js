3c0f1f5bab825bc38568e01eb7c47cfbaa485811
inductor69 <adityakiitdelhi@gmail.com>
2023-01-12 00:50:00 +0530

    updates

--- components/Portfolio.js
+++ components/Portfolio.js
@@ -1,12 +1,12 @@
 import Image from "next/image";
 import Link from "next/link";
 import React from "react";
+import { useEffect , useState } from "react";
 
 export const Portfolio = () => {
   const Projects = [
-
     {
-      id:"0",
+      id: "0",
       title: "Personal Portfolio",
       description:
         "Personal Portfolio showcasing the work of Founders and Designers built with NextJS",
@@ -16,7 +16,7 @@ export const Portfolio = () => {
       controlColor: "#8960b5",
     },
     {
-      id:"2",
+      id: "2",
       title: "GMAT Portal",
       description:
         "This is a full stack portal designed to attempt GMAT mocks, and enhance GMAT scores",
@@ -26,16 +26,7 @@ export const Portfolio = () => {
       controlColor: "#00252F",
     },
     {
-      id:"3",
-      title: "Satpura",
-      description:
-        "Pushing Boundaries On multiple Ideas And Craft",
-      link: "https://satpura-house.vercel.app/",
-      image: "/images/satpura.png",
-      bgColor: "#f0aab1",
-      controlColor: "#cc646e",
-    },{
-      id:"1",
+      id: "1",
       title: "Elekrity",
       description:
         "Beat procrastination and get your personal goals completed as planned!",
@@ -43,40 +34,60 @@ export const Portfolio = () => {
       image: "/images/elekrity2.svg",
       bgColor: "#9df5fd",
       controlColor: "#0129FF",
-    }
-
+    },
+    {
+      id: "3",
+      title: "Satpura",
+      description: "Pushing Boundaries On multiple Ideas And Craft",
+      link: "https://satpura-house.vercel.app/",
+      image: "/images/satpura.png",
+      bgColor: "#f0aab1",
+      controlColor: "#cc646e",
+    },
+   
   ];
+
+  const [currentIndex, setCurrentIndex] = useState(0);
+  let intervalId = null;
+  useEffect(() => {
+    intervalId = setInterval(() => {
+        setCurrentIndex(index => (index + 1) % Projects.length === 0 ? 0 : index + 1);
+    }, 2000);
+    return () => clearInterval(intervalId);
+}, [currentIndex]);
+  console.log(currentIndex)
+
+
   const openInNewTab = (url) => {
     window.open(url, "_blank", "noopener,noreferrer");
   };
   return (
     <div className="min-h-[1rem] mt-[12rem] mb-[25rem] xl:mt-[15rem] xl:mb-[32rem] bg-black relative ">
       <div className="bg-black" style={{ scrollSnapType: "x mandatory" }}>
-        {Projects.map((project, id) =>(
-
-          <div key={(id + 1) % Projects.length} className="" >
+        {Projects.map((project, id) => (
+          <div key={id}>
             <input
               className="sr-only peer"
               type="radio"
               name="carousel"
-              id={`carousel-${((id + 1) % Projects.length)  }`}
-              defaultChecked
+              defaultChecked={currentIndex === id}
+
             />
 
             {/* content #id+1 */}
             <div
               className={
-
                 "2xl:container min-w-[80%]  md:mx-auto absolute top-1/3 left-1/2  transform -translate-x-1/2 -translate-y-1/3  rounded-lg shadow-lg transition-all duration-300 opacity-0 peer-checked:opacity-100 peer-checked:z-10 z-0 "
-
-
               }
             >
               <div
                 className={
                   "py-2 flex rounded-[2rem] flex-wrap-reverse md:flex-nowrap lg:flex-row mx-auto transition ease-in-out duration-700 scale-[1]  hover:scale-[0.99]  max-w-screen-xl xl:max-w-screen-2xl  text-black row align-baseline "
                 }
-                style={{ backgroundColor: `${project.bgColor}`, position: "static"}}
+                style={{
+                  backgroundColor: `${project.bgColor}`,
+                  position: "static",
+                }}
               >
                 <div className="  p-10  md:mt-[15rem] xl:mt-[27rem]">
                   <p className="max-w-md text-3xl md:text-5xl font-semibold ">
@@ -90,7 +101,7 @@ export const Portfolio = () => {
                     className={
                       " button1 mt-10 px-8 py-2 rounded k text-md xl:text-xl bg-black  font-bold  "
                     }
-                    style={{ backgroundColor: `${project.bgColor}`}}
+                    style={{ backgroundColor: `${project.bgColor}` }}
                   >
                     View Project
                   </button>
@@ -113,11 +124,10 @@ export const Portfolio = () => {
                   </div>
                 </div>
                 {/* controls */}
-                <div className="absolute top-1/2 w-full flex justify-between z-20"
-                >
+                <div className="absolute top-1/2 w-full flex justify-between z-20">
                   <label
                     // onClick={(e)=>e.preventDefault()}
-                    htmlFor={`carousel-${(id % Projects.length) }`}
+                    htmlFor={`carousel-${id % Projects.length}`}
                     className={`inline-block cursor-pointer -translate-x-5   bg-white rounded-full shadow-md active:translate-y-0.5`}
                     style={{ color: `${project.controlColor}` }}
                   >
@@ -136,7 +146,7 @@ export const Portfolio = () => {
                   </label>
                   <label
                     // onClick={(e)=>e.preventDefault()}
-                    htmlFor={`carousel-${((id + 2) % Projects.length) }`}
+                    htmlFor={`carousel-${(id + 2) % Projects.length}`}
                     className={`inline-block cursor-pointer   translate-x-5 bg-white rounded-full shadow-md active:translate-y-0.5`}
                     style={{ color: `${project.controlColor}` }}
                   >
